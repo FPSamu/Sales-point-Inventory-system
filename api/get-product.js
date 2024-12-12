e60cc201c9ad95a2efe5d3122596db7e90fd29a1
FPSamu <samuelpia@gmail.com>
2024-12-11 21:42:49 -0600

    Autopopulate FIXED

--- api/get-product.js
+++ api/get-product.js
@@ -31,11 +31,6 @@ export default async function handler(req, res) {
             // Query the database for the given ID
             const result = await collection.findOne({ id: parseInt(id, 10) });
 
-            if (!result) {
-                res.status(404).json({ error: "Product not found" });
-                return;
-            }
-
             res.status(200).json(result);
         } catch (error) {
             console.error("Error fetching data:", error.message);
